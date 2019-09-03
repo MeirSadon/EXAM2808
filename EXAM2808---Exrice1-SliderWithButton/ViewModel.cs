d703707aae84beb45277e461a96fd7c91b29a90b
מאיר סעדון <45739995+MeirSadon@users.noreply.github.com>
2019-09-03 18:33:15 +0300

    Add files via upload

---
+++ EXAM2808---Exrice1-SliderWithButton/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAM2808___Exrice1___SliderWithButton
+{
+    public class ViewModel
+    {
+        public Double SliderWidth { get; set; }
+        public Double SliderHeight { get; set; }
+        public string MyText { get; set; }
+        public DelegateCommand MyCommand { get; set; }
+
+        public ViewModel()
+        {
+            SliderWidth = 70;
+            SliderHeight = 30;
+            MyText = "";
+            var self = this;
+            MyCommand = new DelegateCommand(() => {
+                new Parameters(self).ShowDialog();
+            });
+        }
+    }
+}
