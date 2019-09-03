daaa4511762cb48625b854f2b9bcf3b2a0884291
מאיר סעדון <45739995+MeirSadon@users.noreply.github.com>
2019-09-03 18:37:43 +0300

    Add files via upload

---
+++ EXAM2808-Exrice3-Web/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EXAM2808_Exrice3_Web
+{
+
+    public partial class MainWindow : Window
+    {
+        public ViewModel vm = new ViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = vm;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            vm.MyStopWatch.Restart();
+            string text = "";
+
+            StartWithDispatcherBtn.IsEnabled = false;
+            string myUrl = UrlTxtBx.Text;
+
+            Task.Run(() =>
+            {
+                using (WebClient client = new WebClient())
+                {
+                    text = client.DownloadString(myUrl);
+                }
+
+                int size = ASCIIEncoding.ASCII.GetByteCount(text);
+
+                Action uiwork = () =>
+                {
+                    SizeTxtBlck.Text = $"The Size Of Your Site Is: {size} Bytes.";
+                    StartWithDispatcherBtn.IsEnabled = true;
+                };
+
+                SafeInvoke(uiwork);
+                vm.MyStopWatch.Stop();
+            });
+        }
+
+        private void SafeInvoke(Action work)
+        {
+            if(Dispatcher.CheckAccess())
+            {
+                work.Invoke();
+                return;
+            }
+            Dispatcher.Invoke(work);
+        }
+
+        private void Counter()
+        {
+            while (vm.MyStopWatch.IsRunning)
+            {
+                SafeInvoke(new Action(() => { TimeTxtBlck.Text = vm.ValueOfStopWatch.ToString(); }));
+                Thread.Sleep(10);
+            }
+            TimeTxtBlck.Text = $"You received The Respone At: {vm.ValueOfStopWatch.ToString()} MilliSeconds";
+        }
+    }
+}
---
+++ EXAM2808-Exrice3-Web/ViewModel.cs
@@ -0,0 +1,106 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EXAM2808_Exrice3_Web
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private string startOfUrl;
+        private string myUrl;
+        public string MyUrl { get { return this.myUrl; } set { this.myUrl = value; OnPropertyChanged("MyUrl"); } }
+
+        private string urlSize;
+        public string UrlSize { get { return urlSize; } set { urlSize = value; OnPropertyChanged("UrlSize"); } }
+
+        public string ValueOfStopWatch
+        {
+            get
+            {
+                if (myStopWatch.ElapsedMilliseconds == 0) return "0";
+                if(MyStopWatch.IsRunning == false && myStopWatch.ElapsedMilliseconds != 0) return $"You received The Respone At: {MyStopWatch.ElapsedMilliseconds.ToString()} MilliSeconds";
+                return MyStopWatch.ElapsedMilliseconds.ToString();
+            }
+            set
+            {
+                OnPropertyChanged("ValueOfStopWatch");
+            }
+        }
+
+        private Stopwatch myStopWatch;
+        public Stopwatch MyStopWatch { get { return this.myStopWatch; } set { this.myStopWatch = value;} }
+
+
+        public DelegateCommand StartCommand { get; set; }
+        public bool ProccessIsDone { get; set; } = true;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ViewModel()
+        {
+            startOfUrl = "http";
+            myUrl = "";
+            urlSize = "Please Wait...";
+            myStopWatch = new Stopwatch();
+
+            StartCommand = new DelegateCommand(() =>
+            {
+                CheckUrl(MyUrl);
+            },
+            () =>
+            {
+                return MyUrl.StartsWith(startOfUrl);
+            });
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    StartCommand.RaiseCanExecuteChanged();
+                    Thread.Sleep(500);
+                }
+            });
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    ValueOfStopWatch = MyStopWatch.ElapsedMilliseconds.ToString();
+                }
+            });
+        }
+
+        public async void CheckUrl(string url)
+        {
+            ProccessIsDone = false;
+            MyStopWatch.Restart();
+            WebRequest webRequest = WebRequest.Create(url);
+            WebResponse response = await webRequest.GetResponseAsync();
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                string numberOfCharacters = await reader.ReadToEndAsync();
+                UrlSize = $"The Size Of Your Site Is: {numberOfCharacters.Length.ToString()} Bytes."; 
+            }
+            ProccessIsDone = true;
+            MyStopWatch.Stop();
+        }
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+
+    }
+}
